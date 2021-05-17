4bc2457e27589ae348a8f711729e45dc7681f309
23714 <23714@aomai.com>
2021-05-17 17:40:25 +0800

    修改代码

--- src/main/java/cn/hc/iot/service/TcpServerVerticle.java
+++ src/main/java/cn/hc/iot/service/TcpServerVerticle.java
@@ -1,6 +1,8 @@
 package cn.hc.iot.service;
 
+
 import cn.hc.iot.util.ConcurrentHashMapUtil;
+import cn.hc.iot.util.TlvBox;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
@@ -11,6 +13,9 @@ import io.vertx.core.net.NetSocket;
 import io.vertx.core.parsetools.RecordParser;
 import io.vertx.ext.web.client.WebClient;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -31,34 +36,85 @@ public class TcpServerVerticle extends AbstractVerticle {
         //绑定处理器，当有请求进入时触发
         vertx.createNetServer(options).connectHandler(netSocket -> {
             // 构造parser
-            RecordParser parser = RecordParser.newFixed(3);
+            RecordParser parser = RecordParser.newFixed(8);
             parser.setOutput(new Handler<Buffer>() {
-                short size = -1;
+                int size = -1;
 
                 @Override
                 public void handle(Buffer buffer) {
                     if (-1 == size) {
-                        size = buffer.getShort(1);
+                        size = buffer.getInt(4);
                         parser.fixedSizeMode(size);
 
                     } else {
                         byte[] buf = buffer.getBytes();
-                        System.out.println(new String(buf));
-//                        switch (buf[0]){
-//                            case 0x11:
-//                                Short len =  buffer.getShort(1);
-//                                byte[] bytes = null;
-//                                System.arraycopy(buf,3,bytes,0,len);
-//
-//                                break;
-//                            case 0x30:
-//                                break;
-//                            case 0x40:
-//                                break;
-//                            case 0x60:
-//                                break;
-//                        }
-                        parser.fixedSizeMode(3);
+                        if (buf.length > 2048) {
+                            return;
+                        }
+                        TlvBox tlvBox = TlvBox.parse(buf);
+
+                        switch (buffer.getInt(0)) {
+                            case 0x11:
+                                String payload = tlvBox.getString(0x11);
+                                if (payload.getBytes().length > 2048) {
+                                    return;
+                                }
+                                String[] split = payload.split(",");
+                                String prodKey = split[0];
+                                String deviceName = split[1];
+                                // 创建WebClient，用于发送HTTP或者HTTPS请求
+                                WebClient webClient = WebClient.create(vertx);
+                                // 构造请求的数据
+                                JsonObject data = new JsonObject()
+                                        .put("prodKey", prodKey)
+                                        .put("deviceName", deviceName);
+                                // 以post方式请求远程地址
+                                webClient.postAbs(config().getString("iotos.url")).sendJsonObject(data, handle -> {
+                                    // 处理响应的结果
+                                    if (handle.succeeded()) {
+                                        System.out.println(handle.result().bodyAsJsonObject());
+                                        ConcurrentHashMapUtil.putCache(prodKey + deviceName, netSocket);
+                                        TlvBox tlvBox1 = TlvBox.create();
+                                        tlvBox1.put(0x20,"SUCCESSFUL");
+                                        netSocket.write(Buffer.buffer(tlvBox1.serialize()), ar -> {
+                                            if (ar.succeeded()) {
+                                                System.out.println("响应成功!");
+                                            } else {
+                                                System.err.println("响应失败!");
+                                            }
+                                        });
+                                    } else {
+                                        TlvBox tlvBox2 = TlvBox.create();
+                                        tlvBox2.put(0x21,"AUTHFAILED");
+                                        //回复连接失败
+                                        netSocket.write(Buffer.buffer(tlvBox2.serialize()), ar -> {
+                                            netSocket.close();
+                                        });
+                                    }
+                                });
+                                break;
+                            case 0x30:
+                                byte[] payload2 = tlvBox.getBytes(0x30);
+                                if (payload2.length > 2048) {
+                                    return;
+                                }
+                                //上报
+                                break;
+                            case 0x40:
+                                byte[] payload3 = tlvBox.getBytes(0x40);
+                                if (payload3.length > 0) {
+                                    return;
+                                }
+                                byte[] typeBytes = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(0x50).array();
+                                netSocket.write(Buffer.buffer(typeBytes));
+                                break;
+                            case 0x60:
+                                netSocket.close();
+                                break;
+                        }
+
+
+                        parser.fixedSizeMode(8);
                         size = -1;
                     }
                 }
@@ -115,10 +171,11 @@ public class TcpServerVerticle extends AbstractVerticle {
             });
         }).listen(config().getInteger("tcp.port", 9984), "127.0.0.1", res -> {
             if (res.succeeded()) {
-                System.out.println("Tcp服务端启动成功,端口："+res.result().actualPort());
+                System.out.println("Tcp服务端启动成功,端口：" + res.result().actualPort());
             } else {
                 System.err.println("Tcp服务端启动失败");
             }
         });
     }
+
 }
